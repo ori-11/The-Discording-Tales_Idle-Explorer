d2b0fd8bab09413ae2d5002fbd8715d55f735100
Shmupo <andrewdoan8b@gmail.com>
2024-09-20 19:29:42 -0700

    Fix diagonal pathing bug. Added tile names.

--- scripts/map/MapInputController.gd
+++ scripts/map/MapInputController.gd
@@ -3,15 +3,19 @@ extends Node2D
 signal ValidTileClicked(coord: Vector2i)
 
 @onready var tileMap: TileMapLayer = $"../TileMapLayer"
+# link to where you want to display the hovered tile's name
+@export var hoveredTileLabel: RichTextLabel
 
 func _ready() -> void:
 	add_to_group("MapInputController")
 
 func _input(event: InputEvent) -> void:
-	if Input.is_action_just_pressed("LeftMouseButton"):
-		var tilePos: Vector2i = tileMap.local_to_map(get_local_mouse_position())
-		var tileCellData: TileData = tileMap.get_cell_tile_data(tilePos)
-		if tileCellData != null:
-			if tileCellData.get_custom_data("Traversable"):
-				ValidTileClicked.emit(tilePos)
-				
+	var mouseTilePos: Vector2i = tileMap.local_to_map(get_local_mouse_position())
+	var tileCellData: TileData = tileMap.get_cell_tile_data(mouseTilePos)
+	if tileCellData != null:
+		if Input.is_action_just_pressed("LeftMouseButton"):
+			if tileCellData != null:
+				if tileCellData.get_custom_data("Traversable"):
+					ValidTileClicked.emit(mouseTilePos)
+		else:
+			hoveredTileLabel.text = tileCellData.get_custom_data("TileName")
--- scripts/map/player/PlayerTileMovementComponent.gd
+++ scripts/map/player/PlayerTileMovementComponent.gd
@@ -1,5 +1,7 @@
 extends Node2D
 
+signal targetReached
+
 @onready var playerTile: Node2D = $".."
 
 var path: PackedVector2Array = []
@@ -15,6 +17,9 @@ func _ready() -> void:
 	mapNavigationController = get_tree().get_first_node_in_group("MapNavigationController")
 
 func setTargetCoord(coord: Vector2i) -> void:
+	# Interrupt current travel if a new target is set
+	if traveling:
+		doneTraveling() # Stop traveling and reset the path
 	getPathToCoord(coord)
 
 func getPathToCoord(coord: Vector2i) -> void:
@@ -26,9 +31,15 @@ func getPathToCoord(coord: Vector2i) -> void:
 func setPath(newPath: PackedVector2Array) -> void:
 	path = newPath
 	if !traveling:
-		targetPos = tilemap.to_global(tilemap.map_to_local(path[1]))
-	pathIdx = 1
-	traveling = true
+		# If not traveling, start with the first tile immediately
+		if path.size() > 1:
+			targetPos = tilemap.to_global(tilemap.map_to_local(path[0]))
+			pathIdx = 0
+			traveling = true
+	else:
+		# If already traveling, adjust the target to the first tile in the new path
+		targetPos = tilemap.to_global(tilemap.map_to_local(path[0]))
+		pathIdx = 0
 
 func goToNextTile() -> void:
 	pathIdx += 1
@@ -48,3 +59,4 @@ func _process(delta: float) -> void:
 		playerTile.global_position = playerTile.global_position.move_toward(targetPos, delta * moveSpeed)
 		if playerTile.global_position == targetPos:
 			goToNextTile()
+			targetReached.emit()
